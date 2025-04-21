f0fbf827f3684934fabbddf0d33ece40d633c685
Chinmay Milind Tullu <chinmaytullu15@gmail.com>
2025-04-21 21:44:43 +0530

    updating the test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
-// // This is a basic Flutter widget test.
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility in the flutter_test package. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
-//
+// This is a basic Flutter widget test.
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 //
@@ -27,3 +27,15 @@
 //     expect(find.text('1'), findsOneWidget);
 //   });
 // }
+
+// test/calculator_test.dart
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rust_calculator/main.dart';
+
+void main() {
+  testWidgets('Calculator screen loads', (WidgetTester tester) async {
+    await tester.pumpWidget(const CalculatorApp());
+    expect(find.text('Rust Calculator'), findsOneWidget);
+  });
+}
+
