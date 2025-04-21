6004612fdc9aa9b66a4a654830d1763b8bbeeed4
Chinmay Milind Tullu <chinmaytullu15@gmail.com>
2025-04-21 20:47:42 +0530

    generating .so file

---
+++ rust/calculator/target/aarch64-linux-android/release/deps/calculator.d
@@ -0,0 +1,5 @@
+D:\Flutter_AppDevelopment\rust_calculator\rust\calculator\target\aarch64-linux-android\release\deps\libcalculator.so: src\lib.rs
+
+D:\Flutter_AppDevelopment\rust_calculator\rust\calculator\target\aarch64-linux-android\release\deps\calculator.d: src\lib.rs
+
+src\lib.rs:
---
+++ rust/calculator/target/aarch64-linux-android/release/libcalculator.d
@@ -0,0 +1,1 @@
+D:\Flutter_AppDevelopment\rust_calculator\rust\calculator\target\aarch64-linux-android\release\libcalculator.so: D:\Flutter_AppDevelopment\rust_calculator\rust\calculator\src\lib.rs
